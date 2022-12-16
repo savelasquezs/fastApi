332c7702d07ace507af37f68c7315fd71e01bc14
Santiago Velásquez <santyvano@outlook.com>
2022-12-15 22:35:47 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+# python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastApi
 from fastapi import FastAPI
+from fastapi import Body
 
 
 app=FastAPI()
 
+#models
+class Person(BaseModel):
+    firstName:str
+    lastName:str
+    age: int
+    hairColor: Optional[str]="Black"
+    isMarried:Optional[bool]=None
+
 @app.get("/")
 def home():
     return {"Hello":"World"}
+
+# request and response body
+@app.post("/person/new")
+def createPerson(person:Person=Body(...)):
+    return person
+    
